7b8837daa4ad6134da25bf7b96043002c3a7599e
ShreyaRai20 <shreya200199@gmail.com>
2025-06-22 14:58:44 -0500

    added cards

--- src/components/ui/floating-nav-demo.tsx
+++ src/components/ui/floating-nav-demo.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { FloatingNav } from "./floating-navbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { FocusCardsDemo } from "./focus-card-demo";
 export function FloatingNavDemo() {
   const navItems = [
     {
@@ -32,9 +33,7 @@ export function FloatingNavDemo() {
 const DummyContent = () => {
   return (
     <div className="grid grid-cols-1 h-[40rem] w-full bg-white dark:bg-black relative border border-neutral-200 dark:border-white/[0.2] rounded-md">
-      <p className="dark:text-white text-neutral-600 text-center text-4xl mt-40 font-bold">
-        Scroll back up to reveal Navbar
-      </p>
+      <FocusCardsDemo/>
       <div className="inset-0 absolute bg-grid-black/[0.1] dark:bg-grid-white/[0.2]" />
     </div>
   );
