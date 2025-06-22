61962db30f98ac67f9faf67f33a11b9fb8e56cbe
ShreyaRai20 <shreya200199@gmail.com>
2025-06-21 21:03:34 -0500

    added [sno]

---
+++ src/app/about/[sno]/page.tsx
@@ -0,0 +1,17 @@
+interface AboutSnoProps {
+    params: {
+      sno: string;
+    };
+  }
+  
+  export default function AboutSno({ params }: AboutSnoProps) {
+    const { sno } = params;
+  
+    return (
+      <div>
+        <h1>About Sno</h1>
+        <p>Param sno: {sno}</p>
+      </div>
+    );
+  }
+  
