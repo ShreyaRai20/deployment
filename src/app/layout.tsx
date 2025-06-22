ef37949762787c04dd96fade88c8f00b4fa0f11a
ShreyaRai20 <shreya200199@gmail.com>
2025-06-22 15:12:41 -0500

    added change to nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { NavbarDemo } from "@/components/ui/navbar-demo";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
